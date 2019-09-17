4db8f0c1b6af310c54a0f1c3850f8c3e257ee222
Anton Perekhozhuk <pentatonix1997@gmail.com>
2019-09-17 19:30:19 +0300

    added css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: {
@@ -14,8 +15,26 @@ module.exports = {
             test: /\.js$/,
             loader: 'babel-loader',
             exclude: '/node_modules/'
+        }, {
+            test: /\.css$/,
+            use: [{
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                // you can specify a publicPath here
+                // by default it uses publicPath in webpackOptions.output
+                    publicPath: '../',
+                    hmr: process.env.NODE_ENV === 'development',
+                },
+            },
+            'css-loader',
+            ],
         }]
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+        filename: '[name].css',
+        }),
+    ],
     devServer: {
         overlay: true
     }
