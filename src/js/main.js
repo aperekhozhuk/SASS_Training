58bf4cab263f5364b0880f5f846a20a09f9b6570
Anton Perekhozhuk <pentatonix1997@gmail.com>
2019-09-20 14:41:33 +0300

    bem classes done!

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,26 @@
 window.onload = function() {
-    var loginModal = document.getElementById("loginModal");
-    var loginBtn = document.getElementById("login-button");
-    var closeModal = document.getElementById("loginModal-content-close");
-    var passField = document.getElementById("password-field");
-    var emailField = document.getElementById("email-field");
+    var loginModal = document.getElementById("logModal-container");
+    var loginBtn = document.getElementById("header-button");
+    var closeModal = document.getElementById("logModal-close");
+    var passField = document.getElementById("logModal-form-password-input");
+    var emailField = document.getElementById("logModal-form-email-input");
     var submitBtn = document.getElementById("login-form-submit");
-    var errorContainer = document.getElementById("error-container");
-    var submitInput = document.getElementById("submit-btn");
+    var errorContainer = document.getElementById("logModal-form-error");
+    var submitInput = document.getElementById("logModal-form-submit-input");
+
     loginBtn.onclick = function() {
         loginModal.style.display = "block";
     }
     closeModal.onclick = function() {
         loginModal.style.display = "none";
+    }
+    loginModal.onclick = function(event) {
+        if (event.target == loginModal) {
+            loginModal.style.display = "none";
+        }
     };
-
     passField.onmouseleave = leftField;
     emailField.onmouseleave = leftField;
-    window.onclick = hide_modal;
 
     function leftField() {
         var email = emailField.value;
@@ -45,7 +49,6 @@ window.onload = function() {
     }
 }
 
-
 function validateEmail(email) {
     var re =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email.toLowerCase());
@@ -54,12 +57,6 @@ function validateEmail(email) {
 function validatePassword(pass) {
     let re = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
     return re.test(pass);
-}
-
-function hide_modal(event) {
-    if (event.target == loginModal) {
-        loginModal.style.display = "none";
-    }
 }
 
 
