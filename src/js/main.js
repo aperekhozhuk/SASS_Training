5ed348cd4196314932ac4cb84f8f5a15a410d901
Anton Perekhozhuk <pentatonix1997@gmail.com>
2019-09-24 16:17:51 +0300

    Added validation for second form

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,17 @@ window.onload = function() {
     var loginModal = document.getElementById("logModal-container");
     var loginBtn = document.getElementById("header-button");
     var closeModal = document.getElementById("logModal-close");
+
     var passField = document.getElementById("logModal-form-password-input");
     var emailField = document.getElementById("logModal-form-email-input");
-    var errorContainer = document.getElementById("logModal-form-error");
+    var logErrorContainer = document.getElementById("logModal-form-error");
     var submitInput = document.getElementById("logModal-form-submit-input");
+
+    var signEmailField = document.getElementById("signModal-form-email-input");
+    var signPassField = document.getElementById("signModal-form-password-input");
+    var signConfirmPassField = document.getElementById("signModal-form-confirmPassword-input");
+    var signErrorContainer = document.getElementById("signModal-form-error");
+    var signSubmitInput = document.getElementById("signModal-form-submit-input");
 
     var signUp = document.getElementById("signUp")
     var signModal = document.getElementById("signModal-container");
@@ -38,32 +45,62 @@ window.onload = function() {
     }
     passField.onmouseleave = leftField;
     emailField.onmouseleave = leftField;
+    signPassField.onmouseleave = leftField;
+    signEmailField.onmouseleave = leftField;
+    signConfirmPassField.onmouseleave = leftField;
 
-    function leftField() {
-        var email = emailField.value;
-        var pass = passField.value;
-        //console.log(email,pass);
+    function leftField(event) {
+        var modalType = event.target.id.split('-')[0];
+        var confirmPassword;
+        var email;
+        var pass;
+        var submitBtn;
+        var errorContainer;
+        if (modalType == 'signModal') {
+            confirmPassword = signConfirmPassField.value;
+            if (confirmPassword == "") {
+                return;
+            }
+            email = signEmailField.value;
+            pass = signPassField.value;
+            errorContainer = signErrorContainer;
+            submitBtn = signSubmitInput;
+        } else {
+            email = emailField.value;
+            pass = passField.value;
+            errorContainer = logErrorContainer;
+            submitBtn = submitInput;
+        }
         if (email == "" || pass == "") {
             return;
         }
-
         var flag1 = !validateEmail(email);
         var flag2 = !validatePassword(pass);
-        var message = ""
-        if (( flag1 || flag2)) {
-            submitInput.style.opacity = "0.5"
-            submitInput.disabled = true;
+        var flag3 = ((modalType == 'signModal') && (confirmPassword != pass))
+        var message = "";
+        if (( flag1 || flag2 || flag3)) {
+            submitBtn.style.opacity = "0.5"
+            submitBtn.disabled = true;
             if (flag1) {
-                message += "Bad email adress; "
+                message += "Bad email adress"
+                if (flag2 || flag3) {
+                    message += "; ";
+                }
             }
             if (flag2) {
                 message += "Your password should be at least 8 symbols, contain 1 uppercase, 1 downcase letter, 1 spec symbol, 1 number"
+                if (flag3) {
+                    message += "; ";
+                }
+            }
+            if (flag3) {
+                message += "Your passwords should match!"
             }
             errorContainer.innerHTML = message;
             return;
         }
-        submitInput.style.opacity = "1.0";
-        submitInput.disabled = false;
+        submitBtn.style.opacity = "1.0";
+        submitBtn.disabled = false;
         errorContainer.innerHTML = "";
     }
 }
